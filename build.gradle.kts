cd47871c91af10498d0398cbc237daf157ddb716
qiuzhanghua <qiuzhanghua@icloud.com>
2021-03-23 09:24:00 +0800

    shadow support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     application
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 
@@ -10,13 +11,14 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    // implementation("com.google.guava:guava:29.0-jre")
+    implementation("com.google.guava:guava:30.1.1-jre")
     implementation("org.apache.spark:spark-sql_2.12:3.1.1")
 }
 
 application {
     // Define the main class for the application.
-    mainClass.set("com.example.App")
+    // mainClass.set("com.example.App")
+    mainClassName= "com.example.App"
 }
 
 tasks.test {
