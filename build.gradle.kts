344147c3b7996457065fdc063e5ca20489e69eec
qiuzhanghua <qiuzhanghua@icloud.com>
2021-03-23 09:40:21 +0800

    exclude spark jars from uber jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("com.google.guava:guava:30.1.1-jre")
-    implementation("org.apache.spark:spark-sql_2.12:3.1.1")
+    compileOnly("org.apache.spark:spark-sql_2.12:3.1.1")
 }
 
 application {
